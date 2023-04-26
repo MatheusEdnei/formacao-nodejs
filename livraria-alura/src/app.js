3ec7c2f40df770bb4f03fbb10e5bb53d3d49c1fe
Matheus Ednei <ednei880@gmail.com>
2023-04-26 09:44:13 -0300

    adicionando api livraria-alura

---
+++ livraria-alura/src/app.js
@@ -0,0 +1,24 @@
+import express from 'express'
+
+const app = express()
+
+const livros = [
+    {
+        id: 1,
+        'titulo': 'senhor dos aneis'
+    },
+    {
+        id: 2,
+        'titulo': 'O Hobbit'
+    }
+]
+
+app.get('/', (req, res) => {
+    res.status(200).send('Curso de Node')
+})
+
+app.get('/livros', (req, res) => {
+    res.status(200).json(livros)
+})
+
+export default app
