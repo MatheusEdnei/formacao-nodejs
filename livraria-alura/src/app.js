df05ebd33cfe86ddb7feb75ce8b0731095542ac3
Matheus Ednei <ednei880@gmail.com>
2023-04-28 14:18:36 -0300

    adiciona controller

--- livraria-alura/src/app.js
+++ livraria-alura/src/app.js
@@ -15,12 +15,6 @@ app.get('/', (req, res) => {
     res.status(200).send('Curso de Node')
 })
 
-app.get('/livros', (req, res) => {
-    livros.find((err, livros) => {
-        res.status(200).json(livros)  
-    })
-})
-
 app.get('/livros/:id', (req, res) => {
     const index = buscaLivro(req.params.id)
     res.json(livros[index])
