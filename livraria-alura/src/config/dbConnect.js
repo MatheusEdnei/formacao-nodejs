b3d0795aeb1de7528d249f31d13829c7af9dc455
Matheus Ednei <ednei880@gmail.com>
2023-04-28 16:39:50 -0300

    adiciona variaveis de ambiente e associação entre schemas

--- livraria-alura/src/config/dbConnect.js
+++ livraria-alura/src/config/dbConnect.js
@@ -1,6 +1,13 @@
 import mongoose from 'mongoose'
+import * as dotenv from 'dotenv'
 
-mongoose.connect('mongodb+srv://ednei880:<password>@cluster0.mljrufm.mongodb.net/alura-node?')
+dotenv.config()
+
+const user = process.env.MONGODB_USER
+const password = process.env.MONGODB_PASSWORD
+const node = process.env.MONGODB_NODE
+
+mongoose.connect(`mongodb+srv://${user}:${password}@cluster0.mljrufm.mongodb.net/${node}?`)
 
 const db = mongoose.connection
 
