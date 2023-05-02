6e5b9d55c17f2adc8ff1e6dfea0c0e32637feae6
Matheus Ednei <ednei880@gmail.com>
2023-05-02 19:50:02 -0300

    Adicionando manipulação de erros

--- livraria-alura/src/controllers/autoresController.js
+++ livraria-alura/src/controllers/autoresController.js
@@ -1,17 +1,16 @@
-import mongoose from 'mongoose'
 import autores from '../models/Autor.js'
 
 class AutorController {
-	static listarAutores = async (req, res) => {
+	static listarAutores = async (req, res, next) => {
 		try {
 			const autoresResultado = await autores.find()
 			res.status(200).json(autoresResultado)
 		} catch (erro) {
-			res.status(500).json({ message: 'Erro interno no servidor' })
+			next(erro)
 		}
 	}
 
-	static listarAutorPorId = async (req, res) => {
+	static listarAutorPorId = async (req, res, next) => {
 		const id = req.params.id
 		try {
 			const autoresResultado = await autores.findById(id)
@@ -23,41 +22,38 @@ class AutorController {
 			}
 
 		} catch (erro) {
-			if (erro instanceof mongoose.Error.CastError) {
-				res.status(400).send({message: 'Um ou mais dados fornecidos estão incorretos'})
-			} else {
-				res.status(500).send( { message: 'Erro interno do servidor' } )
-			}
+			next(erro)
 		}
 	}
 
-	static cadastrarAutor = async (req, res) => {
+	static cadastrarAutor = async (req, res, next) => {
 		const autor = new autores(req.body)
 		try {
 			const autorSalvo = await autor.save()
 			res.status(200).send(autorSalvo.toJSON())
 		} catch (erro) {
-			res.status(500).send({ message: `${erro.message} - falha ao cadastrar autor` })
+			next(erro)
+
 		}
 	}
 
-	static atualizarAutor = async (req, res) => {
+	static atualizarAutor = async (req, res, next) => {
 		const id = req.params.id
 		try {
 			const autor = await autores.findByIdAndUpdate(id, { $set: req.body })
 			res.status(200).send({ message: `Autor ${autor} atualizado com sucesso` })
 		} catch (erro) {
-			res.status(500).send({ message: erro.message })
+			next(erro)
 		}
 	}
 
-	static excluirAutor = async (req, res) => {
+	static excluirAutor = async (req, res, next) => {
 		const id = req.params.id
 		try {
 			const autor = await autores.findByIdAndDelete(id)
 			res.status(200).send({ message: `Autor ${autor} removido com sucesso` })
 		} catch (erro) {
-			res.status(500).send({ message: erro.message })
+			next(erro)
 		}
 	}
 
