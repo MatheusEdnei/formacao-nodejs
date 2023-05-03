ffd0e25d13fdf2344e072f5c20f010f5ed6348f5
Matheus Ednei <ednei880@gmail.com>
2023-05-03 08:14:41 -0300

    adicionando tratamento de erros

--- livraria-alura/src/middlewares/manipuladorDeErros.js
+++ livraria-alura/src/middlewares/manipuladorDeErros.js
@@ -1,15 +1,17 @@
 import mongoose from 'mongoose'
+import ErrorBase from '../erros/ErroBase.js'
+import ErroValidacao from '../erros/ErroValidacao.js'
+import RequisicaoIncorreta from '../erros/RequisicaoIncorreta.js'
 
 // eslint-disable-next-line no-unused-vars
 function manipuladorDeErros (erro, req, res, next) {
 	console.log(erro)
 	if (erro instanceof mongoose.Error.CastError) {
-		res.status(400).send({message: 'Um ou mais dados fornecidos estão incorretos'})
+		new RequisicaoIncorreta().enviarResposta(res)
 	} else if (erro instanceof mongoose.Error.ValidationError) {
-		const mensagensErro = Object.values(erro.errors).map(erro => erro.message).join('; ')
-		res.status(400).send({message: `Os seguintes erros foram encontrados: ${mensagensErro}`})
+		new ErroValidacao(erro).enviarResposta(res)
 	} else {
-		res.status(500).send( { message: 'Erro interno do servidor' } )
+		new ErrorBase().enviarResposta(res)
 	}
 }
 
