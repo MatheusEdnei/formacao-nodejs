2ee2e581225d19cb2a47d81e10fea5a7415635f5
Matheus Ednei <ednei880@gmail.com>
2023-05-03 11:29:52 -0300

    adicionando validador global com mongoose

--- livraria-alura/src/models/Livro.js
+++ livraria-alura/src/models/Livro.js
@@ -5,8 +5,21 @@ const livroScrema = new mongoose.Schema(
 		id: { type: String },
 		titulo: { type: String, required: [true, 'O título do livro é obrigatório'] },
 		autor: { type: mongoose.Schema.Types.ObjectId, ref: 'autores', required: [true, 'O autor(a) é obrigatório'] },
-		editora: { type: String, required: [true, 'A editora é obrigatória'] },
-		numeroPaginas: { type: Number }
+		editora: { 
+			type: String, 
+			required: [true, 'A editora é obrigatória'],
+			enum: {
+				values: ['Casa do código', 'Alura'],
+				message: 'A editora {VALUE} fornecida não é um valor permitido'
+			}
+		},
+		numeroPaginas: { 
+			type: Number, 
+			validate: {
+				validator: (valor) => {return valor >= 10 && valor <= 5000},
+				message: 'O número de página deve estar entre 10 e 5000. Valor fornecido: {VALUE}'
+			}
+		}
 	}
 )
 
