dfce93b1a6e31b97c80f83171734c0ca14b022ed
Matheus Ednei <ednei880@gmail.com>
2023-05-04 21:37:19 -0300

    adicionando middleware de paginação

--- livraria-alura/src/controllers/livrosController.js
+++ livraria-alura/src/controllers/livrosController.js
@@ -1,26 +1,14 @@
 import NaoEncontrado from '../erros/NaoEncontrado.js'
-import RequisicaoIncorreta from '../erros/RequisicaoIncorreta'
 import { autores, livros } from '../models/index.js'
 
 class LivroController {
 	static listarLivros = async (req, res, next) => {
 		try {
-			let { limite = 5, pagina = 1, campoOrdenacao = '_id', ordem = -1 } = req.query
-			limite = parseInt(limite)
-			pagina = parseInt(pagina)
-			ordem = parseInt(ordem)
+			const buscaLivros = livros.find()
 
-			if (limite > 0 && pagina > 0) {
-				const livrosResultado = await livros.find().
-					sort({ [campoOrdenacao]: ordem }).
-					skip((pagina - 1) * limite).
-					limit(limite).
-					populate('autor').
-					exec()
-				res.status(200).json(livrosResultado)
-			} else {
-				next(new RequisicaoIncorreta())
-			}
+			req.resultado = buscaLivros
+
+			next()
 		} catch (erro) {
 			next(erro)
 		}
@@ -88,8 +76,10 @@ class LivroController {
 		try {
 			const busca = await processaBusca(req.query)
 			if (busca !== null) {
-				const livrosResultado = await livros.find(busca).populate('autor')
-				res.status(200).send(livrosResultado)
+				const livrosResultado = livros.find(busca).populate('autor')
+
+				req.resultado = livrosResultado
+				next()
 			} else {
 				res.status(200).send([])
 			}
--- livraria-alura/src/routes/autoresRoutes.js
+++ livraria-alura/src/routes/autoresRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import AutorController from '../controllers/autoresController.js'
+import paginar from '../middlewares/paginar.js'
 
 const router = express.Router()
 
-router.get('/autores', AutorController.listarAutores)
+router.get('/autores', AutorController.listarAutores, paginar)
 router.get('/autores/:id', AutorController.listarAutorPorId)
 router.post('/autores', AutorController.cadastrarAutor)
 router.put('/autores/:id', AutorController.atualizarAutor)
