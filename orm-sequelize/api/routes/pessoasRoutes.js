bc9d641ee77b9f586cf09b6fac84907bab937440
Matheus Ednei <ednei880@gmail.com>
2023-08-21 12:27:14 -0300

    adicionando escopos ao controlador

--- orm-sequelize/api/routes/pessoasRoutes.js
+++ orm-sequelize/api/routes/pessoasRoutes.js
@@ -3,11 +3,13 @@ const PessoaController = require("../controllers/PessoaController");
 
 const router = Router();
 
-router.get("/pessoas", PessoaController.pegaTodasAsPessoas);
+router.get("/pessoas", PessoaController.pegaPessoasAtivas);
+router.get("/pessoas/todos", PessoaController.pegaTodasAsPessoas)
 router.get("/pessoas/:id", PessoaController.pegaUmaPessoa);
 router.get("/pessoas/:estudanteId/matricula/:matriculaId", PessoaController.pegaUmaMatricula)
 router.post("/pessoas", PessoaController.criaPessoa);
 router.post("pessoas/:estudanteId/matricula", PessoaController.criaMatricula)
+router.post("/pessoas/:id/restaura", PessoaController.restauraPessoa)
 router.put("/pessoas/:id", PessoaController.atualizaPessoa);
 router.put("/pessoas/:estudanteId/matricula/:matriculaid", PessoaController.atualizaMatricula)
 router.delete("/pessoas/:id", PessoaController.apagaPessoa);
